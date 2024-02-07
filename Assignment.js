2a7ebad8f95ffdf1386890d47b56695c91600cf2
Shatak-the-imposter <helloshatak@gmail.com>
2024-02-07 21:50:28 +0600

    first commit

---
+++ Assignment.js
@@ -0,0 +1,30 @@
+
+// // function calculateMoney(ticketQuantity) {
+// //     if (ticketQuantity >= 0) {
+// //         profit = (ticketQuantity*120)-(500+(8*50));
+// //         return profit;
+// //     }
+// //     else{
+// //         console.log(`The input ${ticketQuantity} is invalid number`)
+// //     }
+// // }
+
+
+
+
+// function checkName(name) {
+
+//     if (typeof name === "string") {
+//         name = name.toLowerCase()
+//         if (name.endsWith("a") || name.endsWith("y") || name.endsWith("i") || name.endsWith("e") || name.endsWith("o") || name.endsWith("u") || name.endsWith("w")) {
+//             return ('good name');
+//         }
+//         else {
+//             return ("bad name")
+//         }
+//     }
+//     else {
+//         return ('Invalid');
+//     }
+// }
+
---
+++ test.js
@@ -0,0 +1,6 @@
+function func() {
+    let str = 'GeeksforGeeks';
+    let index = str.lastIndexOf('for');
+    console.log(index);
+}
+func();
