64c3bd40584849ad296c833d25eaeba0d261eccd
Shatak-the-imposter <helloshatak@gmail.com>
2024-02-08 17:22:42 +0600

    assignment complete and submited

--- Assignment.js
+++ Assignment.js
@@ -1,62 +1,66 @@
 
-// function calculateMoney(ticketSale) {
-//     if (ticketSale >= 0) {
-//         profit = (ticketSale*120)-(500+(8*50));
-//         return profit;
-//     }
-//     else{
-//         return(`The input ${ticketSale} is invalid number`)
-//     }
-// }
+function calculateMoney(ticketSale) {
+    if (ticketSale >= 0) {
+        profit = (ticketSale*120)-(500+(8*50));
+        return profit;
+    }
+    else{
+        return(`The input ${ticketSale} is invalid number`)
+    }
+}
 
 
-// function checkName(name) {
+function checkName(name) {
 
-//     if (typeof name === "string") {
-//         name = name.toLowerCase()
-//         if (name.endsWith("a") || name.endsWith("y") || name.endsWith("i") || name.endsWith("e") || name.endsWith("o") || name.endsWith("u") || name.endsWith("w")) {
-//             return ('good name');
-//         }
-//         else {
-//             return ("bad name")
-//         }
-//     }
-//     else {
-//         return ('Invalid');
-//     }
-// }
-
-
-
-
-// function deleteInvalids(array) {
-//     let newArray = [];
-//     if (Array.isArray(array)) {
-//         for (const number of array) {
-//             if (number /1 === number) {
-//                 newArray.push(number);
-//             }
-//         }
-//         return newArray;
-//     }
-//     else {
-//         return ('Invaild Array')
-//     }
-// }
+    if (typeof name === "string") {
+        name = name.toLowerCase()
+        if (name.endsWith("a") || name.endsWith("y") || name.endsWith("i") || name.endsWith("e") || name.endsWith("o") || name.endsWith("u") || name.endsWith("w")) {
+            return ('good name');
+        }
+        else {
+            return ("bad name")
+        }
+    }
+    else {
+        return ('Invalid');
+    }
+}
 
 
 
-// function password(obj) {
-//     birthYearInString = obj.birthYear.toString()
-//     birthYearSize = birthYearInString.length
-//     if (obj.hasOwnProperty('name') && obj.hasOwnProperty('birthYear') && obj.hasOwnProperty('siteName')&& birthYearSize>=4) {
-//         let pass = `${obj.siteName.replace(obj.siteName[0], obj.siteName[0].toUpperCase())}#${obj.name}@${obj.birthYear}`;
-//         return (pass)
-//     }
-//     else {
-//         return ('invalid')
-//     }
-// }
+
+
+
+function deleteInvalids(array) {
+    let newArray = [];
+    if (Array.isArray(array)) {
+        for (const number of array) {
+            if (number /1 === number) {
+                newArray.push(number);
+            }
+        }
+        return newArray;
+    }
+    else {
+        return ('Invaild Array')
+    }
+}
+
+
+
+function password(obj) {
+    birthYearInString = obj.birthYear.toString()
+    birthYearSize = birthYearInString.length
+    if (obj.hasOwnProperty('name') && obj.hasOwnProperty('birthYear') && obj.hasOwnProperty('siteName')&& birthYearSize>=4) {
+        let pass = `${obj.siteName.replace(obj.siteName[0], obj.siteName[0].toUpperCase())}#${obj.name}@${obj.birthYear}`;
+        return (pass)
+    }
+    else {
+        return ('invalid')
+    }
+}
+
+
 
 
 
@@ -74,10 +78,20 @@ function monthlySavings(arr, livingCost) {
                 savings = totalEarn - livingCost;
             }
         }
-        return savings;
+        // return savings;
+        if (savings < 0) {
+            return "Earn more"
+        }
+        else {
+            return "Your savings is : " + savings
+        }
+
+    }
+    else {
+        return 'invalid input'
     }
 }
 
-let test = monthlySavings([2000, 1000, 1000], 1000)
-console.log(test)
+
+
 
